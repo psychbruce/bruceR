e424017cc84bc76bead1ace2f34e650c81dccf29
Bruce <psychbruce@qq.com>
2021-02-25 16:08:49 +0800

    [0.5.7] added a new function `med_summary()`

--- R/bruceR_regress.R
+++ R/bruceR_regress.R
@@ -1391,6 +1391,83 @@ HLMs_run_parallel=function(formulas.text, data, family=NULL,
 
 
 
+#### Indirect Effect: Model-Based (using the "mediation" package) ####
+
+# edit(mediation:::print.summary.mediate)
+
+#' Better report of mediation analysis based on the \code{mediation} package
+#'
+#' @description
+#' Better report of mediation analysis based on the \code{\link[mediation]{mediation}} package.
+#'
+#' @param model Mediation model built with the \code{mediation} package (see \code{\link[mediation]{mediate}}).
+#' @param nsmall Number of decimal places of output. Default is 3.
+#' @export
+med_summary=function(model, nsmall=3) {
+  x <- model
+  clp <- 100 * x$conf.level
+  cat("\n")
+  cat(sprintf("Causal Mediation Analysis %s\n\n", ifelse(inherits(x,
+    "mediate.tsls"), "using Two-Stage Least Squares", "")))
+  if (x$boot) {
+    cat(sprintf("Nonparametric Bootstrap Confidence Intervals with the %s Method\n\n",
+      ifelse(x$boot.ci.type == "perc", "Percentile", "BCa")))
+  }
+  else {
+    cat(sprintf("%s Confidence Intervals\n\n", ifelse(inherits(x,
+      "mediate.tsls"), "Two-Stage Least Squares", "Quasi-Bayesian")))
+  }
+  if (!is.null(x$covariates)) {
+    cat("(Inference Conditional on the Covariate Values Specified in `covariates')\n\n")
+  }
+  isLinear.y <- ((class(x$model.y)[1] %in% c("lm", "rq")) ||
+    (inherits(x$model.y, "glm") && x$model.y$family$family ==
+      "gaussian" && x$model.y$family$link == "identity") ||
+    (inherits(x$model.y, "survreg") && x$model.y$dist ==
+      "gaussian"))
+  printone <- !x$INT && isLinear.y
+  if (printone) {
+    smat <- c(x$d1, x$d1.ci, x$d1.p)
+    smat <- rbind(smat, c(x$z0, x$z0.ci, x$z0.p))
+    smat <- rbind(smat, c(x$tau.coef, x$tau.ci, x$tau.p))
+    smat <- rbind(smat, c(x$n0, x$n0.ci, x$n0.p))
+    # rownames(smat) <- c("ACME", "ADE", "Total Effect", "Prop. Mediated")
+    rownames(smat) <- c("Indirect Effect", "Direct Effect", "Total Effect", "Prop. Mediated")
+  }
+  else {
+    smat <- c(x$d0, x$d0.ci, x$d0.p)
+    smat <- rbind(smat, c(x$d1, x$d1.ci, x$d1.p))
+    smat <- rbind(smat, c(x$z0, x$z0.ci, x$z0.p))
+    smat <- rbind(smat, c(x$z1, x$z1.ci, x$z1.p))
+    smat <- rbind(smat, c(x$tau.coef, x$tau.ci, x$tau.p))
+    smat <- rbind(smat, c(x$n0, x$n0.ci, x$n0.p))
+    smat <- rbind(smat, c(x$n1, x$n1.ci, x$n1.p))
+    smat <- rbind(smat, c(x$d.avg, x$d.avg.ci, x$d.avg.p))
+    smat <- rbind(smat, c(x$z.avg, x$z.avg.ci, x$z.avg.p))
+    smat <- rbind(smat, c(x$n.avg, x$n.avg.ci, x$n.avg.p))
+    # rownames(smat) <- c("ACME (control)", "ACME (treated)",
+    #   "ADE (control)", "ADE (treated)", "Total Effect",
+    #   "Prop. Mediated (control)", "Prop. Mediated (treated)",
+    #   "ACME (average)", "ADE (average)", "Prop. Mediated (average)")
+    rownames(smat) <- c("Indirect Effect (control)", "Indirect Effect (treated)",
+      "Direct Effect (control)", "Direct Effect (treated)", "Total Effect",
+      "Prop. Mediated (control)", "Prop. Mediated (treated)",
+      "Indirect Effect (average)", "Direct Effect (average)", "Prop. Mediated (average)")
+  }
+  # colnames(smat) <- c("Estimate", paste(clp, "% CI Lower",
+  #   sep = ""), paste(clp, "% CI Upper", sep = ""), "p-value")
+  colnames(smat) <- c("Estimate", paste(clp, "% LLCI",
+    sep = ""), paste(clp, "% ULCI", sep = ""), "pval")
+  # printCoefmat(smat, digits = 3)
+  print_table(smat, nsmall=nsmall)
+  cat("\n")
+  cat("Sample Size Used:", x$nobs, "\n\n")
+  # cat("\n")
+  cat("Simulations:", x$sims, "\n\n")
+  invisible(x)
+}
+
+
 #### Indirect Effect: Sobel Test & MCMC ####
 
 
